dd93557b4ba3dcd5ab5570245a3ed2ff26cb2f6c
Skye Gill <skyerus95@hotmail.co.uk>
2018-07-08 01:25:34 +0100

    height transition for mobile menu

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+if (window.matchMedia("(max-width: 775px)").matches) {
+
+}
+
+let menuBtn = document.getElementById('menu-btn');
+// function openMenu(){
+//     menuBtn.style.height = 'auto';
+
+// }
+// menuBtn.addEventListener('click',openMenu());
+let colNavContainer = document.getElementById('col-nav-container');
+// function toggleMenu(){
+//     if (colNavContainer.style.height === 'auto') {
+//         colNavContainer.style.height = '0';
+//     } else {
+//         colNavContainer.style.height = 'auto';
+//     }
+// }
+
+menuBtn.addEventListener("click",function(){
+    if (colNavContainer.style.height === '150px') {
+        colNavContainer.style.height = '0';
+    } else {
+        colNavContainer.style.height = '150px';
+    }
+});
