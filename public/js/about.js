73391b240d109387f56d47381924fea11d6e268b
Skye Gill <skyerus95@hotmail.co.uk>
2018-07-08 18:55:10 +0100

    js for gallery

---
+++ public/js/about.js
@@ -0,0 +1,21 @@
+let current = document.querySelector('#current');
+let imgs = document.querySelectorAll('.imgs img');
+let opacity = 0.5;
+
+// Set first img opacity
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => img.addEventListener('click',imgClick));
+
+function imgClick(e) {
+    // Reset opacity
+    imgs.forEach(img => (img.style.opacity = 1))
+    current.src = e.target.src;
+
+    // Add fade in 
+    current.classList.add('fade-in');
+
+    // Remove fade-in after 0.5 seconds
+    setTimeout(() => current.classList.remove('fade-in'),500);
+    e.target.style.opacity = opacity;
+}
